3f9e7d57bff1f884726cc6afab759c3b9c5de30d
Iacopo Rozzo <iacopo@sysdig.com>
2025-01-28 10:56:59 +0100

    feat(cri): make CRI lookup retry parameters configurable
    Make the CRI retry lookup configurable, this includes:
    - maximum elapsed time
    - number of retries
    - maximum retry interval

--- userspace/libsinsp/sinsp.h
+++ userspace/libsinsp/sinsp.h
@@ -47,6 +47,7 @@ limitations under the License.
 
 #include <libsinsp/capture_stats_source.h>
 #include <libsinsp/container.h>
+#include <libsinsp/cri_settings.h>
 #include <libsinsp/dumper.h>
 #include <libsinsp/event.h>
 #include <libsinsp/fdinfo.h>
@@ -890,6 +891,7 @@ public:
 	*/
 	void add_cri_socket_path(const std::string& path);
 	void set_cri_timeout(int64_t timeout_ms);
+	void set_cri_retry_parameters(const ::libsinsp::cri::retry_parameters& retry_parameters);
 	void set_cri_async(bool async);
 
 	void set_container_labels_max_len(uint32_t max_label_len);

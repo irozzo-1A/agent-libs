c6fa37305cf5139f9d49e5343ee8dc2f26346712
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2025-03-20 14:45:58 +0100

    fix(userspace/libsinsp): ignore malformed recvmsg ancillary data
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -3755,7 +3755,7 @@ inline void sinsp_parser::process_recvmsg_ancillary_data(sinsp_evt *evt,
 		// sizeof(int) (file descriptor size) and the control message doesn't contain more
 		// data than allowed by kernel constraints.
 		if(data_size % sizeof(int) || fds_len > SCM_MAX_FD) {
-			continue;
+			break;
 		}
 #undef SCM_MAX_FD
 		memcpy(&fds, PPM_CMSG_DATA(cmsg), data_size);
